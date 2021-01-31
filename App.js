27138d481cf39f881cac630f420eaad1640268d6
Luis Juarez <luismanueljf@gmail.com>
2021-01-30 23:13:34 -0600

    change the color to update via codepush

--- App.js
+++ App.js
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
     right: 0,
   },
   body: {
-    backgroundColor: Colors.white,
+    backgroundColor: '#d3d3d3',
   },
   sectionContainer: {
     marginTop: 32,
